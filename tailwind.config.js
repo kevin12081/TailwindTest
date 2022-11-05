bd23ca5958a0672bf7f32f30de42a84c4c2e7339
kevin12081 <kevin90298@gmail.com>
2022-11-05 20:51:37 +0800

    製作Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js'],
-  mode: 'jit',
+  purge: ["./src/**/*.html"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      spacing: {
+        "main-span": "72px",
+      },
       colors: {
-        'primary': '#4f3cc9',
-        'primary-dark': '#4232aa',
-        'secondary': '#ffffff'
-      }
+        "fb-bg": "#18191a",
+        "fb-header": "#242526",
+        "fb-input": "#3a3b3c",
+        "fb-popover": "#3e4042",
+        "fb-active": "#323436",
+        "fb-card": "#242526",
+        fb: "#2e89ff",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [],
+};
